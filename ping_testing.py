3fa7d8f9d86e6bce0b0a6f312f06ee258699024d
kevin <kevindubuche@gmail.com>
2020-11-06 06:57:18 -0500

    first commit

---
+++ ping_testing.py
@@ -0,0 +1,12 @@
+# ----------------------------------------
+# Program  : Efectuer un ping
+# FileName : ping_testing.py
+# I/P 	  : 127.0.0.1
+# O/P 	  :  
+# By       : Kevin J DUBUCHE 
+# ---------------------------------------- 
+
+# Ecrire un programme en python qui permet d'effectuer un ping
+# Contraintes: Port 6332  | use Idle | use socket     
+
+# __________________________________________________________
