298204fdf15840bcb80d7c717394cfc671f2ef2c
kevin <kevindubuche@gmail.com>
2020-11-06 15:27:21 -0500

    adding ttl,time, and source IP

--- ping_testing.py
+++ ping_testing.py
@@ -10,3 +10,31 @@
 # Contraintes: Port 6332  | use Idle | use socket     
 
 # __________________________________________________________
+
+import socket
+from datetime import datetime
+IP = input("Enter the IP address: ")
+t1 = datetime.now()
+ttl = 0.025
+PORT = 8080
+socket.setdefaulttimeout(ttl)
+
+def scan(addr):
+   s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+   socket.setdefaulttimeout(1)
+   result = s.connect_ex((addr,PORT))
+   if result == 0:
+      return 1
+   else :
+      return 0
+
+def ping(addr):
+      if (scan(addr)):
+         t2 = datetime.now()
+         total = t2 - t1
+         print('Succes !!')
+         print ("from {}: ttl={} time={} ms".format(addr,ttl,total))
+      else:
+          print('Port unreachable')
+         
+ping(IP)
